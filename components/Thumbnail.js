5d17ab8e1d8b6d572771d935decd68a28bd433d9
KTELLAWI <koutaiba@msn.com>
2021-04-25 15:43:29 +0300

    margine

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -8,7 +8,7 @@ import {ThumbUpIcon} from '@heroicons/react/outline'
 const  Thumbnail = forwardRef(({result},ref) => {
     const BASE_URL= 'https://image.tmdb.org/t/p/original';
     return (
-        <div ref ={ref} className="group cursor-pointer p-2 transition duration-200 ease-in transform sm: hover:scale-105 hover:z-50   "> 
+        <div ref ={ref} className="group cursor-pointer p-2 ml-3 transition duration-200 ease-in transform sm: hover:scale-105 hover:z-50   "> 
             
             <img
             width={1920}
@@ -27,11 +27,11 @@ const  Thumbnail = forwardRef(({result},ref) => {
             
           />*/}
             <div className="p-2  ">
-                <p className="truncate max-w-md">
+                <p className="truncate max-w-md ml-5">
                     {result.overview}
                 </p>
-                <h2 className=" mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.orginal_name}</h2>
-                <p className="flex items-center opacity-0 group-hover:opacity-100">
+                <h2 className="  ml-5 mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.orginal_name}</h2>
+                <p className="flex items-center ml-5 opacity-0 group-hover:opacity-100">
                     {result.media_type && `${result.media_type} .`}{""}
                     {result.release_date || result.first_air_date} {""}
                     <ThumbUpIcon className="h-5 mx-2"/>{result.vote_count}
