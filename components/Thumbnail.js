014cdc33ab2135d9eeb61949afcb45dae4f6d496
KTELLAWI <koutaiba@msn.com>
2021-04-25 15:34:26 +0300

    removenexjs imagez

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -9,14 +9,23 @@ const  Thumbnail = forwardRef(({result},ref) => {
     const BASE_URL= 'https://image.tmdb.org/t/p/original';
     return (
         <div ref ={ref} className="group cursor-pointer p-2 transition duration-200 ease-in transform sm: hover:scale-105 hover:z-50   "> 
-            <Img
+            
+            <img
+            width={1920}
+            height={1080}
+            src={`
+                ${BASE_URL}${result.backdrop_path} `}
+            />
+            
+            
+          {/*   <Img
             layout="responsive"
             width={1920}
             height={1080}
             src={`
                 ${BASE_URL}${result.backdrop_path} `}
             
-            />
+          />*/}
             <div className="p-2  ">
                 <p className="truncate max-w-md">
                     {result.overview}
